25b368eb81222f21eb4d69f5f3e70707827455df
Benjamin Obafemi <benjaminoba234@gmail.com>
2022-08-10 15:23:19 +0300

    complete footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,95 @@
-import { Box, Image, Spacer, Stack, Text } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Button,
+  Hide,
+  HStack,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Spacer,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
 import Logo from "../assets/images/logo.png";
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import Map from "./Map";
 function Footer() {
+  const emailRef = useRef();
+  const submitEmail = () => {
+    console.log(emailRef.current.value);
+  };
   return (
-    <Box bg={"gray.200"} h={["200px"]}>
+    <Box bg={"#B0B0B0"} color={"white"} p={"10"} mt="auto" w="100%">
       <Stack flexDir={"column"}>
-        <Box display={"flex"} mt="5">
+        <Box display={"flex"}>
           <Box display={"flex"}>
-            <Box
-              ml={"1"}
-              background={"white"}
-              h="30px"
-              w="30px"
-              borderRadius={"full"}
-            >
+            <Box ml={"1"} background={"white"} w="30px" borderRadius={"full"}>
               <Image boxSize="30px" src={Logo} alt="infinity devices" />
             </Box>
-            <Text as={"h1"} ml={"2"}>
+            <Text as={"h1"} fontSize="xl" ml={"2"}>
               Infinity Devices
             </Text>
           </Box>
           <Spacer />
-          <Text>
-            Company Address: Elgon Court Apartment D2, Ralph Bunche Road
-          </Text>
+          <Hide below="md">
+            <Box>
+              <HStack>
+                <Box>
+                  <Link to="/about">About</Link>
+                </Box>
+                <Box>
+                  <Link to="/">Home</Link>
+                </Box>
+                <Box>
+                  <Link to="contact">Contact Us</Link>
+                </Box>
+              </HStack>
+            </Box>
+          </Hide>
         </Box>
 
         <hr />
-        <Box>
-          <Text>Contact Number:0743156011, 0743156011</Text>
-          <Text>Email:infinitydevices@gmail.com</Text>
+        <Box display={"flex"} flexDir={["column", "column", "row"]}>
+          <Box
+            display={"flex"}
+            flexDir="column"
+            fontSize={"lg"}
+            justifyContent="space-between"
+          >
+            <Text mb={"2"}>
+              Company Address: Elgon Court Apartment D2, Ralph Bunche Road
+            </Text>
+            <Text mb={"2"}>Contact Number:0743156011, 0743156011</Text>
+            <Text mb={"2"}>Email:infinitydevices@gmail.com</Text>
+            <InputGroup color={"white"} w={["100%", "100%", "50%"]} size="md">
+              <Input
+                color={"white"}
+                pr="4.5rem"
+                type={"email"}
+                ref={emailRef}
+                placeholder="Enter password"
+              />
+              <InputRightElement width="4.5rem">
+                <Button colorScheme="green" type="submit" onClick={submitEmail}>
+                  Submit
+                </Button>
+              </InputRightElement>
+            </InputGroup>{" "}
+          </Box>
+          <Spacer />
+          <Box pt={["4", "4", "0"]}>
+            <Map />
+          </Box>
         </Box>
-        <Box></Box>
       </Stack>
+      <Box mt={"4"}>
+        <hr />
+        <Text>
+          Copyright 2021-{new Date().getFullYear()}, Infinity Devices.
+        </Text>
+      </Box>
     </Box>
   );
 }
