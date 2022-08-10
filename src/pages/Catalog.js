45b312fdd6d72a82b6b9f18d6a868aea0ce372be
Benjamin Obafemi <benjaminoba234@gmail.com>
2022-08-10 18:05:42 +0300

    styling catalog

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,7 +1,35 @@
+import { Box, Button, Center, Image, Stack, Text } from "@chakra-ui/react";
 import React from "react";
+import ElectricLaparoscopicCutting from "../assets/images/ElectricLaparoscopicCutting.jpeg";
 
+import Clip from "../assets/images/clip.jpeg";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 function Catalog() {
-  return <div>Catalog</div>;
+  return (
+    <Box>
+      <Box>
+        <Image src={Clip} />
+      </Box>
+      <Box pt="10" pb="10">
+        <Center>
+          <Text fontSize={["2xl", "3xl"]} as={"h1"}>
+            Catalog
+          </Text>
+        </Center>
+        <Stack>
+          <Box
+            display={"flex"}
+            flexDir={["column", "column", "row"]}
+            pt={["0", "0", "0"]}
+          >
+            <Box display={"flex"} w={["100%", "100%", "60%"]}>
+              <Image src={ElectricLaparoscopicCutting} />
+            </Box>
+          </Box>
+        </Stack>
+      </Box>
+    </Box>
+  );
 }
 
 export default Catalog;
