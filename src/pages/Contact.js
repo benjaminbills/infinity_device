04d30b79285c7b5d3b7e69fff716f053f8dc27ce
Benjamin Obafemi <benjaminoba234@gmail.com>
2022-08-10 15:29:47 +0300

    configure contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,20 @@
+import { Box, Center, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <Box pt="10">
+      <Center>
+        <Text fontSize={["2xl", "3xl"]} as={"h1"}>
+          Contact Us
+        </Text>
+      </Center>
+      <Stack pt="5" flexDir={["column", "column", "row"]}>
+        <Box>one</Box>
+        <Box>two</Box>
+      </Stack>
+    </Box>
+  );
 }
 
 export default Contact;
